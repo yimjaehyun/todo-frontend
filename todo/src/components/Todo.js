340e1f27fbd0966154ea445866714feba789b725
Jae <jaehyunyim1997@gmail.com>
2021-08-30 20:58:24 -0700

    Add dismissed

--- todo/src/components/Todo.js
+++ todo/src/components/Todo.js
@@ -5,7 +5,8 @@ function Todo({ todos, completeTodo }) {
 	return todos.map((todo, index) => (
 		<div className={todo.isComplete ? 'todo-row complete' : 'todo-row'} key={index}>
 			<div key={todo.id} onClick={() => completeTodo(todo.id)}>
-				{todo.description}
+				{todo.description}  {todo.deadline}
+				<span className={todo.isPassed ? 'passed' : 'not-passed'}>  DISMISSED</span>
 			</div>
 		</div>
 	))
